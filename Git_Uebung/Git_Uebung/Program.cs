12ce5db6a970eb77719c2cac4721b4bd0306961c
Sebastian Rogacki <fiae.srsr@gmail.com>
2018-05-24 09:28:40 +0200

    Initial commit

---
+++ Git_Uebung/Git_Uebung/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git_Uebung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo Welt!");
+            Console.WriteLine("Hello World!");
+
+
+
+
+        }
+    }
+}
